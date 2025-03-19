26c73a47d6ef69cf0e26308c4e23af860ab39d2b
ephraimm-zm <e.mulilo1@alustudent.com>
2025-03-19 07:28:59 +0000

    Commented and added logic

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,13 +4,14 @@
 import requests
 
 def number_of_subscribers(subreddit):
+    """Reddit subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        print(data["data"]["subscribers"])
+        return(data["data"]["subscribers"])
     else:
-        print(f"Error: {response.status_code}")
+        return 0
