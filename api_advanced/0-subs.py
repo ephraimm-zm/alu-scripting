52bc3db49137b599bc22534592444efa1b4306c9
ephraimm-zm <e.mulilo1@alustudent.com>
2025-03-19 10:13:20 +0000

    Changed string type

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,12 +6,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Reddit subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        return(data["data"]["subscribers"])
-    else:
-        return 0
+        return response.json().get("data", {}).get("subscribers", 0)
+    return 0
